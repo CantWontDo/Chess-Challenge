d055ef6030ae931871b69f1db3d98b9c4433765b
CantWontDo <ninjarahul19@gmail.com>
2023-07-29 19:41:31 -0700

    iterative deepening

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -5,65 +5,256 @@ namespace ChessChallenge.Example
 {
     public class EvilBot : IChessBot
     {
-        //                     .  P    K    B    R    Q    K
-        int[] kPieceValues = { 0, 100, 300, 310, 500, 900, 10000 };
-        int kMassiveNum = 99999999;
+        private const sbyte EXACT = 0, LOWERBOUND = -1, UPPERBOUND = 1, INVALID = -2;
+        struct Transposition
+        {
+            public Transposition(ulong zHash, int dep, int eva, Move mv)
+            {
+                zobristHash = zHash;
+                depth = dep;
+                evaluation = eva;
+                FLAG = INVALID;
+                move = mv;
+            }
+            public ulong zobristHash = 0;
+            public int depth = 0;
+            public int evaluation = 0;
+            public sbyte FLAG = INVALID;
+            public Move move = Move.NullMove;
+
+        }
+
+        int maxTime = 40;
+
+        private ulong mask = 0x7FFFFF;
+
+        private Transposition[] transpositionTable;
+
+        public EvilBot()
+        {
+            transpositionTable = new Transposition[mask + 1];
+        }
+
+        // empty, pawn, horse, bishop, castle, queen, king
+        int[] pieceValues = { 0, 100, 320, 330, 500, 900, 20000 };
 
         int mDepth;
-        Move mBestMove;
+        int nodes = 0;
+
+        int infinity = 999999;
 
         public Move Think(Board board, Timer timer)
         {
-            Move[] legalMoves = board.GetLegalMoves();
-            mDepth = 3;
+            Transposition bestMove = transpositionTable[board.ZobristKey & mask];
+            int time = timer.MillisecondsRemaining / maxTime;
+            mDepth = 4;
+            for (int depth = 0; ; depth++)
+            {
+                Search(board, depth, -infinity, infinity);
+                bestMove = transpositionTable[board.ZobristKey & mask];
+                Console.WriteLine(nodes);
 
-            EvaluateBoardNegaMax(board, mDepth, -kMassiveNum, kMassiveNum, board.IsWhiteToMove ? 1 : -1);
-
-            return mBestMove;
+                if (!ShouldDeepen(timer, time))
+                {
+                    break;
+                }
+            }
+            return bestMove.move;
         }
 
-        int EvaluateBoardNegaMax(Board board, int depth, int alpha, int beta, int color)
+
+        int Evaluate(Board board)
         {
-            Move[] legalMoves;
 
-            if (board.IsDraw())
-                return 0;
+            nodes++;
+            int whiteVal = CountMaterial(board, true);
+            int blackVal = CountMaterial(board, false);
 
-            if (depth == 0 || (legalMoves = board.GetLegalMoves()).Length == 0)
+            int eval = whiteVal - blackVal;
+
+            int perspective = board.IsWhiteToMove ? 1 : -1;
+
+            return eval * perspective;
+        }
+
+        int Search(Board board, int depth, int alpha, int beta)
+        {
+
+            Move[] moves = OrderMoves(board);
+
+            if (depth == 0 || moves.Length == 0)
             {
-                // EVALUATE
-                int sum = 0;
 
                 if (board.IsInCheckmate())
-                    return -9999999;
-
-                for (int i = 0; ++i < 7;)
-                    sum += (board.GetPieceList((PieceType)i, true).Count - board.GetPieceList((PieceType)i, false).Count) * kPieceValues[i];
-                // EVALUATE
-
-                return color * sum;
+                {
+                    int rootDistance = mDepth - depth;
+                    return -infinity + rootDistance;
+                }
+                //return Evaluate(board);
+                return SearchForCapture(board, alpha, beta);
             }
 
-            // TREE SEARCH
-            int recordEval = int.MinValue;
-            foreach (Move move in legalMoves)
+            int highestEval = int.MinValue;
+            int startingAlpha = alpha;
+            ref Transposition transposition = ref transpositionTable[board.ZobristKey & mask];
+            if (transposition.zobristHash == board.ZobristKey && transposition.FLAG != INVALID && transposition.depth >= depth && depth < mDepth)
+            {
+
+                if (transposition.FLAG == EXACT) return transposition.evaluation;
+                else if (transposition.FLAG == LOWERBOUND) alpha = Math.Max(alpha, transposition.evaluation);
+                else if (transposition.FLAG == UPPERBOUND) beta = Math.Min(beta, transposition.evaluation);
+                if (alpha >= beta) return transposition.evaluation;
+            }
+
+            if (depth > 0 && board.IsRepeatedPosition())
+            {
+                return 0;
+            }
+            {
+
+            }
+            if (board.IsDraw())
+            {
+                return 0;
+            }
+
+            Move bestMove = moves[0];
+
+            foreach (Move move in moves)
             {
                 board.MakeMove(move);
-                int evaluation = -EvaluateBoardNegaMax(board, depth - 1, -beta, -alpha, -color);
+
+                int value = -Search(board, depth - 1, -beta, -alpha);
+
                 board.UndoMove(move);
 
-                if (recordEval < evaluation)
+                if (highestEval < value)
                 {
-                    recordEval = evaluation;
-                    if (depth == mDepth)
-                        mBestMove = move;
+                    highestEval = value;
+                    bestMove = move;
                 }
-                alpha = Math.Max(alpha, recordEval);
-                if (alpha >= beta) break;
-            }
-            // TREE SEARCH
 
-            return recordEval;
+                alpha = Math.Max(alpha, value);
+                if (alpha >= beta)
+                {
+                    break;
+                }
+            }
+
+            transposition.evaluation = highestEval;
+            transposition.zobristHash = board.ZobristKey;
+            transposition.move = bestMove;
+            if (highestEval < startingAlpha)
+            {
+                transposition.FLAG = UPPERBOUND;
+            }
+            else if (highestEval >= beta)
+            {
+                transposition.FLAG = LOWERBOUND;
+            }
+
+            else
+            {
+                transposition.FLAG = EXACT;
+            }
+            transposition.depth = depth;
+
+            return highestEval;
         }
+
+        Move[] OrderMoves(Board board, bool capturesOnly = false)
+        {
+            Move[] moves = board.GetLegalMoves(capturesOnly);
+            int[] scores = new int[moves.Length];
+            int i = 0;
+            foreach (Move move in moves)
+            {
+                int scoreGuess = 0;
+                Piece start = board.GetPiece(move.StartSquare);
+                Piece end = board.GetPiece(move.TargetSquare);
+
+                if (end.PieceType == PieceType.None)
+                {
+                    scoreGuess -= 10 * (pieceValues[(int)end.PieceType] - pieceValues[(int)start.PieceType]);
+                }
+
+
+                if (move.IsPromotion)
+                {
+                    scoreGuess -= 5 * pieceValues[(int)move.PromotionPieceType];
+                }
+
+                scores[i] = scoreGuess;
+                i++;
+            }
+
+            Array.Sort(scores, moves);
+            //moves.Reverse();
+            /*Console.WriteLine("begin : " + counter);
+            foreach (Move move in moves)
+            {
+
+                Console.WriteLine(move);
+            }
+
+            Console.WriteLine("end : " + counter);*/
+
+            return moves;
+        }
+
+        int SearchForCapture(Board board, int alpha, int beta)
+        {
+            //Console.WriteLine("hi");
+            int score = Evaluate(board);
+
+
+            if (score >= beta)
+            {
+                return beta;
+            }
+            alpha = Math.Max(alpha, score);
+
+
+            Move[] captures = OrderMoves(board, true);
+
+            foreach (Move capture in captures)
+            {
+                board.MakeMove(capture);
+                score = -SearchForCapture(board, -beta, -alpha);
+
+                board.UndoMove(capture);
+
+                if (score >= beta)
+                {
+                    return beta;
+                }
+                alpha = Math.Max(alpha, score);
+            }
+            //Console.WriteLine("beta : " +beta);
+            //Console.WriteLine("alpha :" + alpha);
+            return alpha;
+        }
+        int CountMaterial(Board board, bool isWhite)
+        {
+            int value = 0;
+
+            value += board.GetPieceList(PieceType.Pawn, isWhite).Count * pieceValues[(int)(PieceType.Pawn)];
+            value += (int)(board.GetPieceList(PieceType.Knight, isWhite).Count * pieceValues[(int)(PieceType.Knight)]); /*-
+            ((board.GetPieceList(PieceType.Pawn, !isWhite).Count * 20)));*/
+            value += board.GetPieceList(PieceType.Bishop, isWhite).Count * pieceValues[(int)(PieceType.Bishop)];
+            value += (int)(board.GetPieceList(PieceType.Rook, isWhite).Count * pieceValues[(int)(PieceType.Rook)]); /*+
+            ((board.GetPieceList(PieceType.Pawn, !isWhite).Count * 20)));*/
+            value += (int)(board.GetPieceList(PieceType.Queen, isWhite).Count * pieceValues[(int)(PieceType.Queen)] * 1);
+            value += board.GetPieceList(PieceType.King, isWhite).Count * pieceValues[(int)(PieceType.King)];
+            value += (int)(board.GetLegalMoves().Length * 0.2);
+            return value;
+        }
+
+        bool ShouldDeepen(Timer timer, int maxThinkTime)
+        {
+            int currentThinkTime = timer.MillisecondsElapsedThisTurn;
+            return ((maxThinkTime - currentThinkTime) > currentThinkTime * 2);
+        }
+
     }
 }
--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -8,19 +8,23 @@ public class MyBot : IChessBot
     private const sbyte EXACT = 0, LOWERBOUND = -1, UPPERBOUND = 1, INVALID = -2;
     struct Transposition
     {
-        public Transposition(ulong zobristHash, int eval, int depth)
+        public Transposition(ulong zHash, int dep, int eva, Move mv)
         {
-            this.zobristHash = zobristHash;
-            this.evaluation = eval;
-            this.depth = depth;
+            zobristHash = zHash;
+            depth = dep;
+            evaluation = eva;
             FLAG = INVALID;
+            move = mv;
         }
-
         public ulong zobristHash = 0;
         public int depth = 0;
         public int evaluation = 0;
         public sbyte FLAG = INVALID;
+        public Move move = Move.NullMove;
+
     }
+
+    int maxTime = 30;
 
     private ulong mask = 0x7FFFFF;
 
@@ -34,29 +38,35 @@ public class MyBot : IChessBot
     // empty, pawn, horse, bishop, castle, queen, king
     int[] pieceValues = { 0, 100, 320, 330, 500, 900, 20000 };
 
-    Move bestMove;
     int mDepth;
     int nodes = 0;
 
     int infinity = 999999;
-    bool endgame = false;
 
     public Move Think(Board board, Timer timer)
     {
-
-        if (bestMove.IsCapture)
+        Transposition bestMove = transpositionTable[board.ZobristKey & mask];
+        int time = timer.MillisecondsRemaining / maxTime;
+        mDepth = 4;
+        for (int depth = 0; ; depth++)
         {
-            endgame = Endgame(board);
+            Search(board, depth, -infinity, infinity);
+            bestMove = transpositionTable[board.ZobristKey & mask];
+            Console.WriteLine(nodes);
+
+            if (!ShouldDeepen(timer, time))
+            {
+                break;
+            }
         }
-        mDepth = 5;
-        Search(board, mDepth, -infinity, infinity);
-        Console.WriteLine(nodes);
-        return bestMove;
+        return bestMove.move;
     }
 
 
     int Evaluate(Board board)
     {
+
+        nodes++;
         int whiteVal = CountMaterial(board, true);
         int blackVal = CountMaterial(board, false);
 
@@ -69,35 +79,25 @@ public class MyBot : IChessBot
 
     int Search(Board board, int depth, int alpha, int beta)
     {
-        if(depth > 0 && board.IsRepeatedPosition())
-        {
-            return 0;
-        }
-        {
-            
-        }
-        if(board.IsDraw())
-        {
-            return 0;
-        }
 
         Move[] moves = OrderMoves(board);
 
         if (depth == 0 || moves.Length == 0)
         {
 
-            if(board.IsInCheckmate())
+            if (board.IsInCheckmate())
             {
-                return depth -infinity;
+                int rootDistance = mDepth - depth;
+                return -infinity + rootDistance;
             }
             //return Evaluate(board);
             return SearchForCapture(board, alpha, beta);
         }
 
         int highestEval = int.MinValue;
-
+        int startingAlpha = alpha;
         ref Transposition transposition = ref transpositionTable[board.ZobristKey & mask];
-        if (transposition.zobristHash == board.ZobristKey && transposition.FLAG != INVALID && transposition.depth >= depth)
+        if (transposition.zobristHash == board.ZobristKey && transposition.FLAG != INVALID && transposition.depth >= depth && depth < mDepth)
         {
 
             if (transposition.FLAG == EXACT) return transposition.evaluation;
@@ -106,45 +106,56 @@ public class MyBot : IChessBot
             if (alpha >= beta) return transposition.evaluation;
         }
 
+        if (depth > 0 && board.IsRepeatedPosition())
+        {
+            return 0;
+        }
+        {
+
+        }
+        if (board.IsDraw())
+        {
+            return 0;
+        }
+
+        Move bestMove = moves[0];
+
         foreach (Move move in moves)
         {
-            nodes++;
             board.MakeMove(move);
 
             int value = -Search(board, depth - 1, -beta, -alpha);
 
             board.UndoMove(move);
 
-            if(highestEval < value)
+            if (highestEval < value)
             {
                 highestEval = value;
-                if(depth == mDepth)
-                {
-                    bestMove = move;
-                }
+                bestMove = move;
             }
 
             alpha = Math.Max(alpha, value);
             if (alpha >= beta)
             {
-                break; 
+                break;
             }
         }
 
         transposition.evaluation = highestEval;
         transposition.zobristHash = board.ZobristKey;
-        if(highestEval < alpha)
+        transposition.move = bestMove;
+        if (highestEval < startingAlpha)
         {
             transposition.FLAG = UPPERBOUND;
         }
-        else if(highestEval >= beta)
+        else if (highestEval >= beta)
         {
             transposition.FLAG = LOWERBOUND;
         }
 
         else
         {
-            transposition.FLAG = EXACT; 
+            transposition.FLAG = EXACT;
         }
         transposition.depth = depth;
 
@@ -167,16 +178,18 @@ public class MyBot : IChessBot
                 scoreGuess -= 10 * (pieceValues[(int)end.PieceType] - pieceValues[(int)start.PieceType]);
             }
 
+
             if (move.IsPromotion)
             {
                 scoreGuess -= 5 * pieceValues[(int)move.PromotionPieceType];
             }
+
             scores[i] = scoreGuess;
             i++;
         }
 
         Array.Sort(scores, moves);
-        moves.Reverse();
+        //moves.Reverse();
         /*Console.WriteLine("begin : " + counter);
         foreach (Move move in moves)
         {
@@ -206,7 +219,6 @@ public class MyBot : IChessBot
 
         foreach (Move capture in captures)
         {
-            nodes++;
             board.MakeMove(capture);
             score = -SearchForCapture(board, -beta, -alpha);
 
@@ -222,55 +234,25 @@ public class MyBot : IChessBot
         //Console.WriteLine("alpha :" + alpha);
         return alpha;
     }
-
-    int MopUpEval(Square myKing, Square opposeKing)
-    {
-        int dstCenterFile = Math.Min(3 - opposeKing.File, opposeKing.File - 4);
-        int dstCenterRank = Math.Min(3 - opposeKing.Rank, opposeKing.Rank - 4);
-
-        int dstCenter = dstCenterFile + dstCenterRank;
-
-        int dstKingFile = Math.Abs(myKing.File - opposeKing.File);
-        int dstKingRank = Math.Abs(myKing.Rank - opposeKing.Rank);
-        int dstKing = dstKingFile + dstKingRank;
-
-        return (int)(4.7 * dstCenter + 1.6 * (14 - dstKing));
-    }
-
-    bool Endgame(Board board)
-    {
-        
-        int value = 0;
-        value += board.GetPieceList(PieceType.Knight, false).Count * 3;
-        value += board.GetPieceList(PieceType.Bishop, false).Count * 3;
-        value += board.GetPieceList(PieceType.Rook, false).Count * 5;
-        value += board.GetPieceList(PieceType.Queen, false).Count * 9;
-        Console.WriteLine(value);
-        if(value <= 13)
-        {
-
-            Console.WriteLine("it's ENDGAME!");
-            return true;
-        }
-        return false;
-    }
-
     int CountMaterial(Board board, bool isWhite)
     {
         int value = 0;
 
         value += board.GetPieceList(PieceType.Pawn, isWhite).Count * pieceValues[(int)(PieceType.Pawn)];
-        value += (int)(board.GetPieceList(PieceType.Knight, isWhite).Count * pieceValues[(int)(PieceType.Knight)] * 1);
+        value += (int)(board.GetPieceList(PieceType.Knight, isWhite).Count * pieceValues[(int)(PieceType.Knight)]); /*-
+            ((board.GetPieceList(PieceType.Pawn, !isWhite).Count * 20)));*/
         value += board.GetPieceList(PieceType.Bishop, isWhite).Count * pieceValues[(int)(PieceType.Bishop)];
-        value += (int)(board.GetPieceList(PieceType.Rook, isWhite).Count * pieceValues[(int)(PieceType.Rook)] * 1);
+        value += (int)(board.GetPieceList(PieceType.Rook, isWhite).Count * pieceValues[(int)(PieceType.Rook)]); /*+
+            ((board.GetPieceList(PieceType.Pawn, !isWhite).Count * 20)));*/
         value += (int)(board.GetPieceList(PieceType.Queen, isWhite).Count * pieceValues[(int)(PieceType.Queen)] * 1);
         value += board.GetPieceList(PieceType.King, isWhite).Count * pieceValues[(int)(PieceType.King)];
         value += (int)(board.GetLegalMoves().Length * 0.2);
-
-        if(endgame) 
-            value += MopUpEval(board.GetKingSquare(isWhite), board.GetKingSquare(!isWhite));
-        
         return value;
     }
 
+    bool ShouldDeepen(Timer timer, int maxThinkTime)
+    {
+        int currentThinkTime = timer.MillisecondsElapsedThisTurn;
+        return ((maxThinkTime - currentThinkTime) > currentThinkTime * 2);
+    }
 }
